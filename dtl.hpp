13989922abd8c75d8d888b9c7aac1a0219b585e7
cubicdaiya <cubicdaiya@gmail.com>
2008-12-17 08:38:07 +0900

    update version number
    --HG--
    branch : 0.06

--- dtl.hpp
+++ dtl.hpp
@@ -1,5 +1,5 @@
 /*
-  dtl-0.05 -- Diff Template Library
+  dtl-0.06 -- Diff Template Library
   Copyright(C) 2008-  Tatsuhiko Kubo <cubicdaiya@gmail.com>
 */
 
@@ -727,6 +727,7 @@ namespace dtl {
 	    conflict = true;
 	    return false;
 	  }
+	  // TODO
 	  S = diff_ba->uniPatch(C);
 	}
       }
