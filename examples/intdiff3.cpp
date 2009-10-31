ccb0598287ac6b1cb4cbfdf9a04105008da44784
cubicdaiya <none@none>
2009-10-31 23:28:57 +0900

    for GCC4.3-
    --HG--
    branch : 1.04

--- examples/intdiff3.cpp
+++ examples/intdiff3.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <vector>
 #include <cassert>
+#include <cstdio>
 
 using namespace std;
 
--- examples/strdiff3_test.cpp
+++ examples/strdiff3_test.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <cstdio>
 
 using namespace std;
 
