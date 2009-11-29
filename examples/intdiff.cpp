2dcde761fb23339d99c48c168c37d5c426d242d5
cubicdaiya <none@none>
2009-11-29 16:29:13 +0900

    set size
    --HG--
    branch : 1.04

--- examples/intdiff.cpp
+++ examples/intdiff.cpp
@@ -9,22 +9,23 @@ using dtl::Diff;
 
 int main(int, char**){
   
-  int a[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  int b[10] = {3, 5, 1, 4, 5, 1, 7, 9, 6, 10};
-
-  for (int i=0;i<10;++i) {
+  int a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int b[] = {3, 5, 1, 4, 5, 1, 7, 9, 6, 10};
+  int asiz = sizeof(a) / sizeof(int);
+  int bsiz = sizeof(b) / sizeof(int);
+  for (int i=0;i<asiz;++i) {
     cout << a[i] << " ";
   }
   cout << endl;
-  for (int i=0;i<10;++i) {
+  for (int i=0;i<bsiz;++i) {
     cout << b[i] << " ";
   }
   cout << endl;
   
   typedef int elem;
   typedef vector<int> sequence;
-  sequence A(&a[0], &a[10]);
-  sequence B(&b[0], &b[10]);
+  sequence A(&a[0], &a[asiz]);
+  sequence B(&b[0], &b[bsiz]);
   Diff< elem, sequence > d(A, B);
   d.compose();
   // editDistance
